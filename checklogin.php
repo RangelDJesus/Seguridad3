2a7eaaa321ca297b8469f597b9d995f581cfdff1
RangelDJesus <18151688@aguascalientes.tecnm.mx>
2022-12-01 22:47:44 -0600

    Initial commit

---
+++ checklogin.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+?>
+
+<?php
+
+$conexion = new mysqli("localhost","root","","Seguridade3");
+
+if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+$sql = "SELECT * FROM users WHERE username = '$username'";
+
+
+$result = $conexion->query($sql);
+
+
+if ($result->num_rows > 0) {     }
+	
+ 
+  $row = $result->fetch_array(MYSQLI_ASSOC);
+if ($password==$row['password']) { 
+
+ 
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    $_SESSION['start'] = time();
+    $_SESSION['expire'] = $_SESSION['start'] + (2*15);
+
+    echo "Bienvenido! " . $_SESSION['username'];
+    echo "<br><br><a href=panel-control.php>Panel de Control</a>"; 
+    header('Location: panel-control.php');
+
+ } else { 
+   echo "Username o Password estan incorrectos.";
+
+   echo "<br><a href='index.php'>Volver a Intentarlo</a>";
+ }
+ mysqli_close($conexion); 
+ ?>
---
+++ panel-control.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+
+} else {
+   echo "Inicia Sesion para acceder a este contenido.<br>";
+   echo "<br><a href='index.php'>Login</a>";
+   header('Location: index.php');//redirige a la página de login si el usuario quiere ingresar sin iniciar sesion
+
+
+exit;
+}
+
+$now = time();
+
+if($now > $_SESSION['expire']) {
+session_destroy();
+header('Location: index.php');//redirige a la página de login, modifica la url a tu conveniencia
+echo "Tu sesion a expirado,
+<a href='index.php'>Inicia Sesion</a>";
+exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Perfil</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style type="text/css"> 
+  a:link 
+  { 
+  text-decoration:none; 
+  } 
+  </style>
+
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Welcome <?php echo  $_SESSION['username'];?></h1>
+  <p>Keep your profile updated!</p> 
+  <a href="logout.php"><button type="button" class="btn btn-success"> Cerrar Sesion</button></a>
+</div>
+  
+
+</div>
+
+</body>
+</html>
