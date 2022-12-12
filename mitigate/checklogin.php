0785d5416cb56279f3bcb1aac6fd1d9bb7924206
RangelDJesus <18151688@aguascalientes.tecnm.mx>
2022-12-12 02:16:49 -0600

    Mitigacion xd

---
+++ mitigate/checklogin.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+?>
+
+<?php
+
+$CaptchaToken = $_POST['g-recaptcha-response'];
+
+if($CaptchaToken!=''){
+  $secret = "6Lc3M3IjAAAAAExazO0Ym9L7Y0S3OY3d_aI5wcFB";
+  $ip = $_SERVER['REMOTE_ADDR'];
+  $var = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$CaptchaToken&remoteip=$ip");
+
+  $array = json_decode($var,true);
+  if($array['success']){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $conexion = new mysqli("localhost","root","","Seguridade3");
+
+    if ($conexion->connect_error) {
+      die("La conexion falló: " . $conexion->connect_error);
+    }
+ 
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+
+    $result = $conexion->query($sql);
+    if ($result->num_rows  <1 ) {
+      echo "El usuario no existe.";
+      header("HTTP/1.1 404 Not Found");
+      echo "<br><a href='index.php'>Volver a Intentarlo</a>";
+    }else{
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      if ($password==$row['password']) { 
+ 
+      $_SESSION['loggedin'] = true;
+      $_SESSION['username'] = $username;
+      $_SESSION['start'] = time();
+      $_SESSION['expire'] = $_SESSION['start'] + (2*15);
+
+      echo "Bienvenido! " . $_SESSION['username'];
+      echo "<br><br><a href=panel-control.php>Panel de Control</a>"; 
+      header('Location: panel-control.php');
+      } else { 
+        echo "Username o Password estan incorrectos.";
+        header("HTTP/1.1 404");
+        echo "<br><a href='index.php'>Volver a Intentarlo</a>";
+      }
+      mysqli_close($conexion);   
+    }
+  }
+}else{
+  echo "Tienes que validar el captcha para continuar.";
+  echo "<br><a href='index.php'>Volver a Intentarlo</a>";
+}
+?>
